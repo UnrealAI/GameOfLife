ab27d94d92c0b212188cda427555525684e6b8a5
jeffdo <jedo@calpoly.edu>
2018-06-10 12:28:29 -0700

    updating to have agents interact each other

--- src/main.py
+++ src/main.py
@@ -1,27 +1,152 @@
-import pygame, sys
+import pygame, sys, numpy as np
 import traits
+from time import sleep
+from sklearn.neighbors import NearestNeighbors
+import networkx as nx
+import scipy
+import copy
+
 
 from pygame.locals import *
 
+white = [255, 255, 255]
+
+class Environment:
+    def __init__(self, WINDOWSIZE=None):
+        self.WINDOWSIZE = WINDOWSIZE
+
 pygame.init()
 
-SCREEN = pygame.display.set_mode((300,300))
+env = Environment(WINDOWSIZE=(800,800))
+SCREEN = pygame.display.set_mode(env.WINDOWSIZE)
 
 pygame.display.set_caption('Game of Life')
+SCREEN.fill(white)
+
+agents = {}
+n_born = 0
+
+agents = pygame.sprite.Group()
+terrain = pygame.sprite.Group()
 
 def generate_agents():
-    # pos = (100,50,20,20)
-    # rad = 20
-    # pygame.draw.circle(SCREEN , (0,255,0), (100,50), rad)
-    agent = traits.Organism()
-    pygame.draw.circle(SCREEN, agent.color, (100,50), agent.size)
+    global agents
+    for i in range(50):
+        agents.add(traits.Organism(np.random.randint(20,50),np.random.randint(400),np.random.randint(50),np.random.randint(200),pos=(np.random.randint(800),np.random.randint(800)),health=np.random.randint(2000,5000),env=env))
 
+def visualizeAgent(agent):
+    pygame.draw.circle(SCREEN, agent.color, agent.pos, agent.size)
+
+def visualizeAgents():
+    for agent in agents:
+        visualizeAgent(agent)
+
+def assign_species_names():
+    global agents
+
+    X = [agent.color for agent in agents]
+    """for agent in agents:
+        print(agent)
+    print(X)"""
+    nn = NearestNeighbors(metric='cosine',algorithm='brute')
+    nn.fit(X)
+    nbrs_graph = nn.radius_neighbors_graph(X,radius=0.001)
+    _, connected_components = scipy.sparse.csgraph.connected_components(nbrs_graph)
+    #print(connected_components)
+    print(connected_components)
+    for agent,cluster in zip(agents,connected_components):
+        agent.update_species_name('species%d'%cluster)
+
+
+def movement(agent):
+    key = pygame.key.get_pressed()
+    theta = np.random.rand()*2.*np.pi
+    if 0:
+        theta = None
+        if key[pygame.K_LEFT]:
+            theta = -np.pi
+        if key[pygame.K_RIGHT]:
+            theta = 0
+        if key[pygame.K_UP]:
+            theta = -np.pi/2.
+        if key[pygame.K_DOWN]:
+            theta = np.pi/2
+    agent.move(theta)
+    return agent
+
+def create_background():
+    # Five rows of blocks
+    rows = 8
+    cols = 8
+    block_width = env.WINDOWSIZE[0] / rows
+    block_height = env.WINDOWSIZE[1] / cols
+    top = 100
+    for row in range(rows):
+        # 32 columns of blocks
+        for column in range(int(cols/2)):
+            # Create a block (color,x,y)
+            block = traits.Terrain("land", column * (block_width), row * block_height)
+            terrain.add(block)
+        for column in range(int(cols/2),cols):
+            # Create a block (color,x,y)
+            block = traits.Terrain("water", column * (block_width), row * block_height)
+            terrain.add(block)
+        # Move the top of the next row down
+        top += block_height
+
+def collision(organism1,organism2):
+    if organism1.species_name != organism2.species_name:
+        pass
+
+def traverse_collisions():
+    global n_born
+    agent_dead_list = []
+    new_agent_list = []
+    for agent in agents:
+        agents_hit = pygame.sprite.spritecollide(agent,agents, False, pygame.sprite.collide_circle)
+        for agent2 in agents_hit:
+            agent.interaction(agent2)
+        agent.is_dead()
+        if agent.dead:
+            agent_dead_list.append(agent)
+        if agent.mated: # mate with multiple members too, no clone
+            new_agent = traits.Organism(size=agent.size, attack=agent.attack, speed=agent.speed, defense=agent.defense,total_calories=agent.total_calories,pos=agent.pos,health=np.random.randint(2000,5000),env=env)
+            new_agent.pos += (np.random.rand(2)*new_agent.speed/4.).astype(np.int)
+            new_agent_list.append(new_agent) # this is a clone for now, but should update the DNA
+    #print(new_agent_list)
+    for agent in agent_dead_list:
+        agent.kill()
+        del agent
+
+    if len(agents) <= 150:
+        for agent in new_agent_list:
+            n_born += 1
+            agents.add(agent)
+
+
+create_background()
 generate_agents()
 
+counter = 0
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
 
+    sleep(0.01)
+    for agent in agents:
+        agent = movement(agent)
+
+    # for agent in agents:
+    #     print(agents[agent].pos)
+
+    SCREEN.fill(white)
+    terrain.draw(SCREEN)
+    visualizeAgents()
     pygame.display.update()
+    traverse_collisions()
+    print("counter {} pop {} born {}".format(counter,len(agents),n_born))
+    counter += 1
+    if n_born % 100 == 0 :# change to 0 when ready to implement
+        assign_species_names()
--- src/traits.py
+++ src/traits.py
@@ -1,16 +1,10 @@
 import numpy as np
-class Organism:
+import pygame
 
-    size = 100
-    attack = 100 #{'bite':100,'lunge':55}#{attack_type:damage, ...}
-    speed= 100   #{'land':100,'water':}#{speed_type:value}
-    defense=100
-    color = np.array([128.,128.,128.])
-    total_calories=1000
-    energy_consumption = 200
-    pos = np.array([0,0])
-
-    def __init__(self, size=100, attack=100, speed=100, defense=100, total_calories=1000, energy_consumption=200, pos=np.array([0,0])):
+class Organism(pygame.sprite.Sprite):
+    def __init__(self, size=100, attack=100, speed=10, defense=100, total_calories=1000, energy_consumption=200, health = 5000, pos=np.array([0,0]), attackMoves={}, speedMod={"land":100, "water":100}, env=None):
+        pygame.sprite.Sprite.__init__(self)
+        self.species_name = 'species0'
         self.size = size
         self.attack = attack
         self.speed = speed
@@ -20,12 +14,117 @@ class Organism:
         self.attribute_array = np.array([self.size,self.attack,self.speed,self.defense,self.total_calories,self.energy_consumption])
         self.color = np.minimum(np.array([np.dot(self.attribute_array,[0,1,0,0,0,0]),np.dot(self.attribute_array,[0,0,1,0,0,0]),np.dot(self.attribute_array,[0,0,0,1,0,0])]),[255,255,255])
         self.pos = pos
+        self.health = 5000
+        self.attackMoves = attackMoves
+        self.env = env
+        self.radius = self.size
+        self.dead = False
+        self.mated = False
+        self.speedMod = speedMod
+
+    @property
+    def rect(self):
+        return pygame.Rect(self.pos[0], self.pos[1], self.radius*2, self.radius*2)
 
     def return_color(self):
         return self.color
 
     def move(self, theta):
-        self.pos += np.array([np.cos(theta),np.sin(theta)])*self.speed
+        if theta != None:
+            self.pos += (np.array([np.cos(theta),np.sin(theta)])*self.speed).astype(int)
+            # clip it right where
+            self.pos = (self.pos[0].clip(0,self.env.WINDOWSIZE[0]), self.pos[1].clip(0,self.env.WINDOWSIZE[1]))
+
+    def update_species_name(self, species_name):
+        self.species_name = species_name
+
+    def mate(self, organism2):
+        # will make twins for now
+        if np.random.rand() < 0.01:
+            self.mated = True
+
+    def interaction(self, organism2):
+        self.mated = False
+        if self.species_name != organism2.species_name:
+            self.is_attacked(organism2)
+        else:
+            self.mate(organism2)
+
+    def is_attacked(self, organism2):
+        self.health += np.minimum(0,self.defense-organism2.attack)
+
+    def is_dead(self):
+        if self.health <= 0.:
+            self.dead = True
+
+    def defend(self):
+        pass
+
+    # def render(self):
+    #     pygame.draw.circle(SCREEN, agent.color, agent.pos, agent.size)
+
+
+class Controller:
+
+    def __init__(organism1, organism2):
+        self.organism1 = organism1
+        self.organism2 = organism2
+
+    def interaction(self):
+        if self.organism1:
+            pass
+        #similarity = np.dot(self.organism1.color,self.organism2.color)/(np.linalg.norm(self.organism1.color)*np.linalg.norm(self.organism2.color))
+
+    # mate, retain same species name, mutate crossover
 
 
 # calories used: 2*size + 100*feet/second
+
+
+class Terrain(pygame.sprite.Sprite):
+    white = (255, 255, 255)
+    block_width = 100
+    block_height = 100
+
+    def __init__(self,type,x,y):
+        super().__init__()
+        self.image = pygame.Surface([self.block_width, self.block_height])
+        self.type = type
+        self.color = self.getColor(self.type)
+        self.image.fill(self.color)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+    def properties(type):
+        movementMod = {
+            "land": None,
+            "water": None
+        }
+        if(type == "land"):
+            movementMod["land"] = 1.0
+            movementMod["water"] = 0.0
+        elif(type == "water"):
+            movementMod["land"] = 0.0
+            movementMod["water"] = 1.0
+        return {
+            "color":self.color,
+            "movementMod": movementMod
+        }
+
+    def getColor(self,type):
+        color = None
+        if(type == "land"):
+            color = (50,200,50)
+        elif(type == "water"):
+            color = (50,50,200)
+        return color
+# planned interaction:
+# based off where they are positioned, they will be affected by terrain.
+# how to lay terrain?
+
+    # general plan of terrain
+    # water
+    # land
+    # muddy land
+    # grassland
